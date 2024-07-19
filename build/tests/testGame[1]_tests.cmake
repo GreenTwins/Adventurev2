3ea7c943f10bece241cceeda0fceb3f7304b3dab
Aaron <aaron.fogle@gmail.com>
2024-07-18 20:07:07 -0400

    tracking build files

--- build/tests/testGame[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[makePlayerTest.CreatePlayer]=]  C:/Users/aaron/source/repos/Adventure/adventurev2/clonedAdv/advpacket/build/tests/Debug/testGame.exe [==[--gtest_filter=makePlayerTest.CreatePlayer]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[makePlayerTest.CreatePlayer]=]  PROPERTIES WORKING_DIRECTORY C:/Users/aaron/source/repos/Adventure/adventurev2/clonedAdv/advpacket/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  testGame_TESTS makePlayerTest.CreatePlayer)
